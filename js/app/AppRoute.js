aa5782d2baa674e44991e5a5cfd70e3ee3f2d80e
hamza alawi <hamza_hm@hotmail.be>
2020-06-18 13:05:40 +0100

    SPA Ultimate Structure for MQL Platform Project 3.0 All components are included from their html file !! HEADER AND FOOTER STATIC CONTENT NOW IN INDEX HOME IS ADDED NOW first ADDING LOADING -> bugs fix 5
    PHONE MENU FIX

--- js/app/AppRoute.js
+++ js/app/AppRoute.js
@@ -3,19 +3,9 @@
  * @param component
  */
 function route(component = '') {
-    if(component === '') {
-        if(sessionStorage.getItem('component') !== null) {
-            component = sessionStorage.getItem('component');
-        } else {
-            component = 'Home';
-        }
-    } else {
-        sessionStorage.setItem('component', component);
-    }
-    current_component = component;
+    initComponent(component);
     load();
     initializer = setInterval(run, 10);
-    $('#loader').style.display = 'block';
 }
 
 /**
