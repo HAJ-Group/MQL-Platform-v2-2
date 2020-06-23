31e88bcbaa67c5bfd09da936ac9ce84fa5fbf873
hamza alawi <hamza_hm@hotmail.be>
2020-06-24 00:38:25 +0100

    home version 2.0

--- js/views/HomeComponent.js
+++ js/views/HomeComponent.js
@@ -110,8 +110,8 @@ function HomeMain() {
 	views['home'] = new HomeComponent(service);
 	views['home'].printStats();
 	views.home.startPresenter();
-	views.home.printNews();
-	views.home.setNewsRoutes();
+	// views.home.printNews();
+	// views.home.setNewsRoutes();
 	// stays last
 	addTitleIcon('resources/pictures/Home/title-logo.png', false, 'home');
 	detect_subContent_trigger_left_bar('home');
