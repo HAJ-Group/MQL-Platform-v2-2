7162ecc1b6031ed59a04a6600f3d906db485345f
jaouad <jaouadaitassou@gmail.com>
2020-06-25 22:48:47 +0100

    Replacing static html content in ActivityComponent by dynamic content using ElementFactory

--- js/views/ActivityComponent.js
+++ js/views/ActivityComponent.js
@@ -19,6 +19,7 @@ ActivityComponent.prototype.printActivityList = function () {
 	}
 };
 /*--------------------------------------------------------------------------------------------------------------------*/
+/*
 ActivityComponent.prototype.printSemesters = function(){
 	let subjectZone = $('#zone');
 	for (let i = 0; i < this.service.size(); i++) {
@@ -86,7 +87,124 @@ ActivityComponent.prototype.printSemesters = function(){
 				'<div class="cards-container content-card">' + cards + '</div>' +
 			'</div>';
 	}
+}; */
+
+/*--------------------------------------------------------------------------------------------------------------------*/
+ActivityComponent.prototype.printSemesters = function(){
+	let subjectZone = $('#zone');
+	for (let i = 0; i < this.service.size(); i++) {
+		let semesterI = this.service.get(i);
+		let semesterName = '';
+		switch (i + 1) {
+			case 1: semesterName = 'Le premier semestre'; break;
+			case 2: semesterName = 'Le deuxième semestre'; break;
+			case 3: semesterName = 'Le troisième semestre'; break;
+			case 4: semesterName = 'Le quatrième semestre (Stage pré-embauche)'; break;
+		}
+
+		let ulModulesElement = buildElement('ul', null);
+		let moduleImage = '';
+		for (let j = 0; j < semesterI.modules.length; j++) {
+			moduleImage = semesterI.modules[0];
+			if (j === 0) continue;
+			ulModulesElement.appendChild(buildElement('li', 'M' + (j) + '(' + semesterI.modules[j] + ')'));
+		}
+
+		let activitiesElement = [];
+		let activityImage = '';
+		for (let j = 0; j < semesterI.activity.length; j++) {
+			activityImage = semesterI.activity[0];
+			if(j === 0) continue;
+			activitiesElement.push(buildParagraph( [buildSPAN(),
+				semesterI.activity[j]]
+			));
+		}
+
+		let cards = [];
+		for (let j = 1; j <= 3; j++) {
+			if (j === 1){
+				cards.push(buildDIV(
+					buildParagraph(semesterI.description[1]), cls('semester-description')
+				));
+
+			}
+			let divCard;
+			let divCardText;
+			let divModules;
+			let divActivities;
+			let divCardFooter;
+			if (j === 2){
+
+				if(i !==3) {
+					divCard = buildDIV(
+						buildIMG(moduleImage, '', cls('card-image')),
+						cls('card'));
+
+					divCardText = buildDIV([
+						buildDIV('Modules', cls('card-subject')),
+					], cls('card-text'));
+
+					divModules = buildDIV(null, cls('subject'));
+
+					divModules.appendChild(ulModulesElement);
+
+					divCardText.appendChild(divModules);
+
+					divCardFooter = buildDIV(
+						buildIMG('../../resources/pictures/logo-mql2.png', '',
+							cls('logo-mql')),
+						cls('card-footer'));
+
+					divCard.appendChild(divCardText);
+					divCard.appendChild(divCardFooter);
+
+					cards.push(divCard);
+				}
+			}
+			if (j === 3){
+				divCard = buildDIV(
+					buildIMG(activityImage, '', cls('card-image')),
+					wrapIC('card', 'card'));
+
+				divCardText = buildDIV([
+					buildDIV('Objectifs', cls('card-subject')),
+				], cls('card-text'));
+
+				divActivities = buildDIV(null, cls('subject'));
+				for (let activity of activitiesElement) {
+					divActivities.appendChild(activity);
+				}
+
+				divCardText.appendChild(divActivities);
+
+				divCardFooter = buildDIV(
+					buildIMG('../../resources/pictures/logo-mql2.png', '',
+						cls('logo-mql')),
+					cls('card-footer'));
+
+				divCard.appendChild(divCardText);
+				divCard.appendChild(divCardFooter);
+
+				cards.push(divCard);
+			}
+		}
+
+		console.log(cards);
+		let bigContainer = buildDIV(
+			buildDIV(semesterName, wrapIC('collapse-' + (i + 1), 'title-top-cards collapsible')),
+			cls('big-container')
+		);
+
+		let cardContainer = buildDIV(null, cls('cards-container content-card'));
+		for (let card of cards){
+			cardContainer.appendChild(card);
+		}
+
+		bigContainer.appendChild(cardContainer);
+		subjectZone.appendChild(bigContainer);
+	}
 };
+
 /*--------------------------------------------------------------------------------------------------------------------*/
 ActivityComponent.prototype.updateView = function () {
 	if(window.innerWidth > 700){
